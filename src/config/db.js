95a70d7687e12cca3b70ba83b51e9a14c0ce274c
Dev <128153292+DEVASHISH-PRASAD@users.noreply.github.com>
2024-01-18 16:47:06 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+Mongo_uri=process.env.MONGO_URI||"mongodb://localhost:27017/my_database";
 const connectToDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(Mongo_uri);
     console.log("Server is running on:", conn.connection.host);
   } catch (error) {
     console.error(error.message);
